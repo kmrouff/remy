64ba627e3d5e19d5451f6938264a7de572972fc8
kmrouff <37094608+kmrouff@users.noreply.github.com>
2026-07-21 14:11:43 +0200

    Point Buy me a coffee at the real page

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,5 +1,4 @@
-// TODO: swap in the real Buy Me a Coffee page once it exists.
-export const SUPPORT_URL = 'https://buymeacoffee.com/remy'
+export const SUPPORT_URL = 'https://buymeacoffee.com/remythechef'
 
 export const CONTACT_EMAIL = 'hello@remythechef.com'
 
